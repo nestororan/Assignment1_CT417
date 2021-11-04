27178e2759b7b63332edeab6510f95eb615d75aa
nestororan <91605052+nestororan@users.noreply.github.com>
2021-11-04 22:56:06 +0000

    Add files via upload

---
+++ gradleproject1/src/main/java/gradleproject1/myTest.java
@@ -0,0 +1,28 @@
+//package com.github.EigerEx;
+
+import gradleproject1.Student;
+import org.junit.*;
+import java.util.*;
+import org.joda.time.DateTime;
+
+public class myTest
+{
+    private Student s;
+    
+    List<String> courses;
+    List<String> modules;
+    
+    @Test
+    public void testStudent() {
+        
+        String DOB="13/05/1964";
+        
+        courses.add("Maths science");
+        courses.add("Biotech");
+        modules.add("Biochemistry");
+        modules.add("Mechanical Mapping");
+        
+        s= new Student("Padraic", 57, DOB, 163283, courses, modules);
+        Assert.assertEquals((Object)s.getName(), (Object)"Padraic");
+    }
+}
